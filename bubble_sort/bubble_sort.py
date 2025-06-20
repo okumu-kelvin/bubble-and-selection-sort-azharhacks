f2d27c8c6b5f8782957d66dcedafeb210ad94d80
azharhacks <azharabeid5@gmail.com>
2025-06-20 11:32:54 +0300

    bubble,selection

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,15 @@
-def bubble_sort(unsorted_list):
+values = [64, 34, 25, 12, 22, 11, 90]
+def bubble_sort(numbers):
     # TODO: Implement bubble sort
+    
+    for i in range(len(numbers)):
+        for j in range(len(numbers)-1):
+            if numbers[j] > numbers[j + 1]:
+                x=numbers[j+1]
+
+                numbers[j+1]=numbers[j]
+                numbers[j]=x
+    return numbers
     pass
+sorted_list = bubble_sort(values)
+print("Sorted list:", sorted_list)
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,22 @@
-def selection_sort(arr):
+values=[1, 34, 25, 12, 22, 11, 34]
+def selection_sort(numbers):
     # TODO: Implement selection sort
+     
+    for i in range(len(numbers)):
+        print("i",numbers[i])
+        
+        min_index = i
+        for j in range( i+1,len(numbers)):
+            if numbers[j] < numbers[min_index]:
+                min_index = j
+                print("j",numbers[j])
+
+        # Swap the found minimum with the first unsorted element
+        x=numbers[i]
+        numbers[i]=numbers[min_index]
+        numbers[min_index]=x
+        
+    return numbers
     pass
+sorted_list = selection_sort(values)
+print("Sorted list:", sorted_list)
